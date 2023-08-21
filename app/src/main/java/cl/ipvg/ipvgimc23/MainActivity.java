3061536a6f244f33074155687a1d519b1edd8b13
DiegoVGcl <diego.chavez@virginiogomez.cl>
2023-08-21 10:10:18 -0400

    Actualizacion APP

--- app/src/main/java/cl/ipvg/ipvgimc23/MainActivity.java
+++ app/src/main/java/cl/ipvg/ipvgimc23/MainActivity.java
@@ -5,30 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditTest peso,altura;
-    private Button aceptar;
-    private TextView resultado;
+     EditText peso,altura;
+     Button aceptar;
+     TextView resultado;
+     TextView mensaje;
 
-    private Float fpeso,faltura,fresultado;
+     Float fpeso,faltura,fresultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        peso = (EditTest) findViewById(R.id.etPeso);
-        altura = (EditTest) findViewById(R.id.etAltura);
-        aceptar = (Button) findViewById(R.id.tvAceptar);
-        resultado = (TextView) findViewById(R.id.tvResultado);
+            peso = (EditText) findViewById(R.id.etPeso);
+            altura = (EditText) findViewById(R.id.etAltura);
+            aceptar = (Button) findViewById(R.id.tvAceptar);
+            resultado = (TextView) findViewById(R.id.tvResultado);
+            mensaje = (TextView) findViewById(R.id.tvMensaje);
 
         aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fpeso=peso.getText.
+
+                try {
+                    fpeso = Float.parseFloat(peso.getText().toString());
+                    faltura = Float.parseFloat(altura.getText().toString());
+                    // resultado=(peso/(altura*altura))
+
+                    fresultado = fpeso / (faltura * faltura);
+                    resultado.setText("Resultado IMC: " + fresultado);
+                } catch (NumberFormatException e)
+                {
+                    resultado.setText("Error en el Resultado del IMC");
+                }
+
+                if (fresultado > 30) {
+                    mensaje.setText("SobrePeso");
+                }
+                else
+                {
+                    mensaje.setText("Normal");
+                }
             }
+
+
         });
     }
 }
